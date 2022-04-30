176f25f5c283acbb87e420f8d8d7230401ef0747
Romenig Lima Damasio <romenigld@gmail.com>
2022-04-30 19:51:03 +0200

    LiveComponents

--- lib/poeticoins_web/live/crypto_dashboard_live.ex
+++ lib/poeticoins_web/live/crypto_dashboard_live.ex
@@ -4,46 +4,56 @@ defmodule PoeticoinsWeb.CryptoDashboardLive do
   import PoeticoinsWeb.ProductHelpers
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, trades: %{}, products: [])
+    socket = assign(socket, products: [])
     {:ok, socket}
   end
 
   def handle_info({:new_trade, trade}, socket) do
-    socket =
-      socket
-      |> update(:trades, &Map.put(&1, trade.product, trade))
+    send_update(PoeticoinsWeb.ProductComponent,
+                id: trade.product,
+                trade: trade)
 
     {:noreply, socket}
   end
 
   def handle_event("add-product", %{"product_id" => product_id} = _params, socket) do
-    [exchange, currency_pair] = String.split(product_id, ":")
-    product = Product.new(exchange, currency_pair)
+    product = product_from_string(product_id)
     socket = maybe_add_product(socket, product)
     {:noreply, socket}
   end
 
-  def handle_event("filter-products", %{"search" => search}, socket) do
-    products =
-      Poeticoins.available_products()
-      |> Enum.filter(fn product ->
-        String.downcase(product.exchange_name) =~ String.downcase(search) or
-        String.downcase(product.currency_pair) =~ String.downcase(search)
-      end)
-
-      socket = assign(socket, products: products)
+  def handle_event("add-product", _, socket) do
     {:noreply, socket}
   end
+
+  def handle_event("remove-product", %{"product-id" => product_id}= _params, socket) do
+    product = product_from_string(product_id)
+    socket = update(socket, :products, & List.delete(&1, product))
+    {:noreply, socket}
+  end
+
+  defp product_from_string(product_id) do
+    [exchange_name, currency_pair] = String.split(product_id, ":")
+    Product.new(exchange_name, currency_pair)
+  end
+
+  # def handle_event("filter-products", %{"search" => search}, socket) do
+  #   products =
+  #     Poeticoins.available_products()
+  #     |> Enum.filter(fn product ->
+  #       String.downcase(product.exchange_name) =~ String.downcase(search) or
+  #       String.downcase(product.currency_pair) =~ String.downcase(search)
+  #     end)
+
+  #     socket = assign(socket, products: products)
+  #   {:noreply, socket}
+  # end
 
   def add_product(socket, product) do
     Poeticoins.subscribe_to_trades(product)
 
     socket
     |> update(:products, &(&1 ++ [product]))
-    |> update(:trades, fn trades ->
-      trade = Poeticoins.get_last_trade(product)
-      Map.put(trades, product, trade)
-    end)
   end
 
   defp maybe_add_product(socket, product) do
