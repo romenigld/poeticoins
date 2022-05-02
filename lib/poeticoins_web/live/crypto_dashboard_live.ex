611d5c9f098735303deebd3f7dd77e9ab923d911
Romenig Lima Damasio <romenigld@gmail.com>
2022-05-02 19:44:36 +0200

    unsubscribe the product from trades when is removed

--- lib/poeticoins_web/live/crypto_dashboard_live.ex
+++ lib/poeticoins_web/live/crypto_dashboard_live.ex
@@ -30,6 +30,7 @@ defmodule PoeticoinsWeb.CryptoDashboardLive do
 
   def handle_event("remove-product", %{"product-id" => product_id}= _params, socket) do
     product = product_from_string(product_id)
+    Poeticoins.unsubscribe_from_trades(product)
     socket = update(socket, :products, &List.delete(&1, product))
     {:noreply, socket}
   end
